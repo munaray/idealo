ba6c038e505907d5bb565315942923c8ee239a63
munaray <saabdulsalam1215225@gmail.com>
2024-12-11 19:31:07 +0100

    Initial commit

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const mongoose = require("mongoose");
 const XLSX = require("xlsx");
 
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/idealo_scraper");
+mongoose
+	.connect(process.env.DB_URI)
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((error) => {
+		console.error("Error connecting to MongoDB:", error.message);
+	});
 
 // Mongoose Schema
 const productSchema = new mongoose.Schema(
